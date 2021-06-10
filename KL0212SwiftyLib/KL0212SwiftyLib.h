aa993bdf06ff8398655755c3332a4cb3d821a6ad
Khải Lê <lekhai0212@gmail.com>
2021-06-10 14:54:50 +0700

    Initial project setup

---
+++ KL0212SwiftyLib/KL0212SwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  KL0212SwiftyLib.h
+//  KL0212SwiftyLib
+//
+//  Created by Khai Leo on 6/10/21.
+//  Copyright © 2021 KhaiLe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KL0212SwiftyLib.
+FOUNDATION_EXPORT double KL0212SwiftyLibVersionNumber;
+
+//! Project version string for KL0212SwiftyLib.
+FOUNDATION_EXPORT const unsigned char KL0212SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KL0212SwiftyLib/PublicHeader.h>
+
+
---
+++ KL0212SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KL0212SwiftyLibExamples
+//
+//  Created by Khai Leo on 6/10/21.
+//  Copyright © 2021 KhaiLe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
